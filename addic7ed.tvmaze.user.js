e6a50593023f0dd6f7ca9ef86efa8dd8403e9609
vBm <the.vbm@gmail.com>
2015-10-08 14:07:48 +0200

    TVMaze with addic7ed flavor Adds button below the "mark as watched/watched" one that opens correct addic7ed page for that specific episode.

---
+++ addic7ed.tvmaze.user.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name        TVMaze Subtitles
+// @namespace   tvmaze
+// @description Link addic7ed subs
+// @icon        http://tvmazecdn.com/images/favico/favicon.ico
+// @author      vBm <vbm@omertabeyond.com>
+// @oujs:author vBm
+// @license     The MIT License (MIT)
+// @supportURL  https://github.com/vBm/snippets/issues
+// @include     http://www.tvmaze.com/episodes/*
+// @version     0.1
+// @date        08/10/2015
+// @grant       none
+// ==/UserScript==
+
+var showInfoName = $('div#general-info-panel').find('p').first().find('a').text();
+
+var showInfoRegex = /Number\:.*(\d+).*\n\s+Episode.(\d+)/g;
+var match = showInfoRegex.exec($('div#general-info-panel p')[0].innerHTML);
+
+var showInfoSeason = match[1];
+var showInfoEpisode = match[2];
+var showInfoEpisodeName = $('header h1').text();
+
+// Due to different names either one of pages use we have to create an array to circumvent around it.
+var addic7ed = {
+	'Legends': 'Legends_(2014)',
+    'The Flash': 'The Flash (2014)'
+};
+
+showInfoName = addic7ed[showInfoName] || showInfoName;
+
+var subtitlesUrl = 'http://www.addic7ed.com/serie/'+showInfoName+'/'+showInfoSeason+'/'+showInfoEpisode+'/'+showInfoEpisodeName;
+console.log(subtitlesUrl);
+
+$('#main-img').append(
+	$('<a>').attr({
+        id: 'watching',
+		class: 'radius small button secondary addic7ed',
+		target: '_blank',
+        style: 'margin-top: 3px'
+	}).text(' Addic7ed subs').click(
+		function() {
+			window.open(subtitlesUrl);
+	})
+);
+
+$('<i class="fa fa-download"></i>').prependTo('.addic7ed');
