25ef6bac18d4d92ff97c8041c6738770e5c40eba
vBm <the.vbm@gmail.com>
2017-01-17 14:52:32 +0100

    [ended] Switch to https in header and use ES6 template literal

--- ended.tvmaze.user.js
+++ ended.tvmaze.user.js
@@ -1,20 +1,23 @@
 // ==UserScript==
-// @name        TVMaze Ended Shows
-// @namespace   tvmaze
-// @description Distinguish ended shows on TVMaze with *
-// @icon        http://tvmazecdn.com/images/favico/favicon.ico
-// @author      vBm <vbm@omertabeyond.com>
-// @oujs:author vBm
-// @license     The MIT License (MIT)
-// @supportURL  https://github.com/vBm/snippets/issues
-// @include     http://www.tvmaze.com/watchlist*
-// @version     0.1
-// @date        31/01/2016
-// @grant       none
+// @name                TVMaze Ended Shows
+// @namespace           tvmaze
+// @description         Distinguish ended shows on TVMaze with *
+// @icon                https://tvmazecdn.com/images/favico/favicon.ico
+// @author              vBm <vbm@omertabeyond.com>
+// @oujs:author         vBm
+// @license             The MIT License (MIT)
+// @contributionURL     https://www.paypal.me/thevbm/3
+// @contributionAmount  €3.00
+// @supportURL          https://github.com/vBm/snippets/issues
+// @include             http://www.tvmaze.com/watchlist*
+// @include             https://www.tvmaze.com/watchlist*
+// @version             0.2
+// @date                17/01/2017
+// @grant               none
 // ==/UserScript==
 
 $.ajax({
-    url: '//' + $(location).attr('hostname') + '/showstatus',
+    url: `//${$(location).attr('hostname')}/showstatus`,
     cache: false,
 }).done(function(data) {
     var showNames = [];
@@ -26,7 +29,7 @@ $.ajax({
         });
         $('H2').each(function() {
             if ($.inArray($(this).text(), showNames) > -1 ) {
-                $(this).html('* '+ $(this).html());
+                $(this).html(`* ${$(this).html()}`);
             }
         });
     }
