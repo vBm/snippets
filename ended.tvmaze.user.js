af9aef3b13f8a9fab279668371741d6ca90e9ede
Goran Džaferi <vBm@users.noreply.github.com>
2021-02-28 18:49:13 -0700

    [tvmaze][ended] Fix loading of the script
    Size method has been removed in jQuery 3.0.
    
    https://api.jquery.com/size/

--- ended.tvmaze.user.js
+++ ended.tvmaze.user.js
@@ -3,7 +3,7 @@
 // @namespace           tvmaze
 // @description         Distinguish ended shows on TVMaze with *
 // @icon                https://tvmazecdn.com/images/favico/favicon.ico
-// @author              vBm <vbm@omertabeyond.com>
+// @author              vBm <the.vbm@gmail.com>
 // @oujs:author         vBm
 // @license             The MIT License (MIT)
 // @contributionURL     https://www.paypal.me/thevbm/3
@@ -11,8 +11,8 @@
 // @supportURL          https://github.com/vBm/snippets/issues
 // @include             http://www.tvmaze.com/watchlist*
 // @include             https://www.tvmaze.com/watchlist*
-// @version             0.2
-// @date                17/01/2017
+// @version             0.3
+// @date                28/02/2021
 // @grant               none
 // ==/UserScript==
 
@@ -21,7 +21,7 @@ $.ajax({
     cache: false,
 }).done(function(data) {
     var showNames = [];
-    if ($(data).find('[data-title^=Status]').size() !== 0 ) {
+    if ($(data).find('[data-title^=Status]').length !== 0 ) {
         $(data).find('[data-title^=Status]').each(function() {
             if ($(this).text() === 'Ended') {
                 showNames.push($(this).parent().find('[data-title^=Show]').text());
